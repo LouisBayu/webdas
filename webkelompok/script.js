78f37919b1d4e333835bb3aaf77edcad490f4593
LouisBayu <111201911773@mhs.dinus.ac.id>
2020-11-13 21:22:59 +0700

    finishing Tugas Web Kelompok

--- webkelompok/script.js
+++ webkelompok/script.js
@@ -27,3 +27,12 @@ function navBarButton() {
           overlay.style.visibility = "hidden";
       }
   }
+
+// Input Hanya angka
+  function hanyaAngka(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+
+        return false;
+    return true;
+    }
